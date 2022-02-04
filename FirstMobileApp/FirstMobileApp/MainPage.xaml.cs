f843ba2cd3f955c75e5a9d59a3f2117868c77ec1
Adorjan Cizmar <adorjan.cizmar@gmail.com>
2022-02-04 14:58:11 +0100

    fixed the bug in displaying result

--- FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
+++ FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
@@ -1,205 +1,201 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xamarin.Forms;
-
-namespace FirstMobileApp
+﻿namespace FirstMobileApp
 {
+    using System;
+    using System.Collections.Generic;
+    using System.ComponentModel;
+    using System.Data;
+    using System.Linq;
+    using System.Text;
+    using System.Threading.Tasks;
+    using Xamarin.Forms;
+
     public partial class MainPage : ContentPage
     {
+        private bool displayingResult = false;
+
         public MainPage()
         {
-            InitializeComponent();
+            this.InitializeComponent();
 
-            SubscribeNumberAndBracketButtons();
-            SubscibeOperandButtons();
-
-        }
-
-        private void SubscibeOperandButtons()
-        {
-            List<Button> allOperands = new List<Button>()
-            {
-                Division,Multiplication,Substraction,Addition,Dot
-            };
-            allOperands.ForEach(button => button.Clicked += OperandsAndDotClicked);
-        }
-
-        private void OperandsAndDotClicked(object sender, EventArgs e)
-        {
-            Button button = (Button)sender;
-
-            if ( !IsLastInputOperand() )
-            {
-                CheckLabelLenght();
-                Display.Text += button.Text;
-
-            }else if(button.Text == "-")
-            {
-                Display.Text += "(-";
-            }
-            
-        }
-
-        private bool IsLastInputOperand()
-        {
-            string lastInput;
-            if (Display.Text.Length > 1)
-            {
-                lastInput = Display.Text.Substring(Display.Text.Length - 1, 1);
-            }
-            else
-            {
-                lastInput = Display.Text;
-            }
-
-            if(!isOperand(lastInput))
-            {
-                return false; 
-            }
-            return true;
-        }
-        private bool isOperand(string charToCheck)
-        {
-            if (charToCheck == Division.Text ||
-                charToCheck == Multiplication.Text ||
-                charToCheck == Substraction.Text ||
-                charToCheck == Addition.Text ||
-                charToCheck == Dot.Text)
-            {
-                return true;
-            }
-            return false;
-        }
-        private void CheckLabelLenght()
-        {
-            if (Display.Text.Length > 10)
-            {
-                Display.FontSize = 50;
-            }
-            else
-            {
-                Display.FontSize = 85;
-            }
+            this.SubscribeNumberAndBracketButtons();
+            this.SubscibeOperandButtons();
         }
 
         private void SubscribeNumberAndBracketButtons()
         {
             List<Button> allNumbers = new List<Button>()
             {
-                Num0,Num1,Num2,Num3,Num4,Num5,Num6,Num7,Num8,Num9,OpenBracket,ClosedBracket
+                this.Num0, this.Num1, this.Num2, this.Num3, this.Num4,
+                this.Num5, this.Num6, this.Num7, this.Num8, this.Num9,
+                this.OpenBracket, this.ClosedBracket,
             };
-            allNumbers.ForEach(button => button.Clicked += NumberClicked);
+            allNumbers.ForEach(button => button.Clicked += this.Number_Clicked);
         }
 
-        private void NumberClicked(object sender, EventArgs e)
+        private void Number_Clicked(object sender, EventArgs e)
         {
-            CheckLabelLenght();
+            this.CheckLabelLenght();
             Button button = (Button)sender;
-            Display.Text = Display.Text == "0" ? button.Text : Display.Text += button.Text;
-            
-            if(button.Text == ")")
+
+            // typing numbers after result is displayed
+            if (this.displayingResult)
             {
-                EmptyBracketChecker();
-                OpenBracketPresentChecker();
-            }else if(button.Text == "(")
-            {
-                OperandBeforeBracketChecker();
+                this.Display.Text = button.Text;
+                this.displayingResult = false;
             }
-            
+            else
+            {
+                this.Display.Text = this.Display.Text == "0" ? button.Text : this.Display.Text += button.Text;
+            }
+
+            if (button.Text == ")")
+            {
+                this.EmptyBracketChecker();
+                this.OpenBracketPresentChecker();
+            }
+            else if (button.Text == "(")
+            {
+                this.OperandBeforeBracketChecker();
+            }
+        }
+
+        private void SubscibeOperandButtons()
+        {
+            List<Button> allOperands = new List<Button>()
+            {
+                this.Division, this.Multiplication, this.Substraction, this.Addition, this.Dot,
+            };
+            allOperands.ForEach(button => button.Clicked += this.OperandsAndDotClicked);
+        }
+
+        private void OperandsAndDotClicked(object sender, EventArgs e)
+        {
+            Button button = (Button)sender;
+
+            if (!this.IsLastInputOperand())
+            {
+                this.displayingResult = false;
+                this.CheckLabelLenght();
+                this.Display.Text += button.Text;
+            }
+            else if (button.Text == "-")
+            {
+                this.Display.Text += "(-";
+            }
+        }
+
+        private bool IsLastInputOperand()
+        {
+            string lastInput;
+
+            if (this.Display.Text.Length > 1)
+            {
+                lastInput = this.Display.Text.Substring(this.Display.Text.Length - 1, 1);
+            }
+            else
+            {
+                lastInput = this.Display.Text;
+            }
+
+            if (!this.IsOperand(lastInput))
+            {
+                return false;
+            }
+
+            return true;
+        }
+
+        private bool IsOperand(string charToCheck)
+        {
+            return charToCheck == this.Division.Text ||
+                   charToCheck == this.Multiplication.Text ||
+                   charToCheck == this.Substraction.Text ||
+                   charToCheck == this.Addition.Text ||
+                   charToCheck == this.Dot.Text;
+        }
+
+        private void CheckLabelLenght()
+        {
+            if (this.Display.Text.Length > 10)
+            {
+                this.Display.FontSize = 50;
+            }
+            else
+            {
+                this.Display.FontSize = 85;
+            }
         }
 
         private void OpenBracketPresentChecker()
         {
-           
-            if (Display.Text.Length > 1 && !Display.Text.Contains("("))
+            if (this.Display.Text.Length > 1 && !this.Display.Text.Contains("("))
             {
-               
-                Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
-                
+                this.Display.Text = this.Display.Text.Substring(0, this.Display.Text.Length - 1);
             }
         }
 
         private void OperandBeforeBracketChecker()
         {
-             
-            if (Display.Text.Length > 1)
+            if (this.Display.Text.Length > 1)
             {
-                string lastInput = Display.Text.Substring(Display.Text.Length - 2, 1);
-                if (!isOperand(lastInput))
+                string lastInput = this.Display.Text.Substring(this.Display.Text.Length - 2, 1);
+
+                if (!this.IsOperand(lastInput))
                 {
-                    Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
+                    this.Display.Text = this.Display.Text.Substring(0, this.Display.Text.Length - 1);
                 }
             }
         }
 
         private void EmptyBracketChecker()
         {
-            string lastInput;
-            if (Display.Text.Length > 1)
+            if (this.Display.Text.Length > 1)
             {
-                lastInput = Display.Text.Substring(Display.Text.Length - 2, 2);
-                if(lastInput == "()")
+                string lastInput = this.Display.Text.Substring(this.Display.Text.Length - 2, 2);
+
+                if (lastInput == "()")
                 {
-                    Display.Text = Display.Text.Substring(0, Display.Text.Length - 2);
+                    this.Display.Text = this.Display.Text.Substring(0, this.Display.Text.Length - 2);
                 }
             }
             else
             {
-                Display.Text = "0";
-
+                this.Display.Text = "0";
             }
-
         }
 
         private void C_Clicked(object sender, EventArgs e)
         {
-            Display.Text = "0";
-            CheckLabelLenght();
+            this.Display.Text = "0";
+            this.CheckLabelLenght();
         }
+
         private void Back_Clicked(object sender, EventArgs e)
         {
-            if (Display.Text.Length > 1)
-            {
-                Display.Text = Display.Text.Substring(0, Display.Text.Length - 1);
-            }
-            else
-            {
-                Display.Text = "0";
-            }
-            CheckLabelLenght();
+            this.Display.Text = this.Display.Text.Length > 1 ?
+                this.Display.Text.Substring(0, this.Display.Text.Length - 1) : "0";
+
+            this.CheckLabelLenght();
         }
-
-
 
         private void Equals_Clicked(object sender, EventArgs e)
         {
-            if (Display.Text.Contains("("))
+            if (this.Display.Text.Contains("(") && !this.Display.Text.Contains(")"))
             {
-                if (Display.Text.Contains(")"))
-                {
-                    Display.Text = Evaluate(Display.Text).ToString();
-                }
+                return;
             }
-            else
-            {
-                Display.Text = Evaluate(Display.Text).ToString();
-            }
-            
+
+            this.Display.Text = this.Evaluate(this.Display.Text).ToString();
+            this.displayingResult = true;
         }
+
         private double Evaluate(string expression)
         {
             var loDataTable = new DataTable();
             var loDataColumn = new DataColumn("Eval", typeof(double), expression);
             loDataTable.Columns.Add(loDataColumn);
             loDataTable.Rows.Add(0);
-            return (double)(loDataTable.Rows[0]["Eval"]);
+            return (double)loDataTable.Rows[0]["Eval"];
         }
     }
-
-    
 }
