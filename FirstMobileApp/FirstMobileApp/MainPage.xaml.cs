1718f898ef81f25ceab4a00032443731708d9593
Adorjan Cizmar <adorjan.cizmar@gmail.com>
2022-02-12 10:34:38 +0100

    check box implemented

--- FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
+++ FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
@@ -14,7 +14,18 @@ namespace FirstMobileApp
         {
             InitializeComponent();
 
-            //some comment to test git
+            FirstLabel.Text = Check1.IsChecked ? "Checked" : "NotChecked";
+            SecondLabel.Text = Check2.IsChecked ? "Checked" : "NotChecked";
+        }
+
+        private void CheckBox_CheckedChanged(object sender, CheckedChangedEventArgs e)
+        {
+            FirstLabel.Text = e.Value ? "Checked" : "Not Checked";
+        }
+
+        private void CheckBox_CheckedChanged_1(object sender, CheckedChangedEventArgs e)
+        {
+            SecondLabel.Text = e.Value ? "Checked" : "Not Checked";
         }
     }
 }
