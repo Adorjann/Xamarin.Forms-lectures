cf36e8e0f66636afc69d3a5938ea94ff57c9b78d
Adorjan Cizmar <adorjan.cizmar@gmail.com>
2022-02-23 23:49:40 +0100

    ui and themes implemented

--- FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
+++ FirstMobileApp/FirstMobileApp/MainPage.xaml.cs
@@ -1,20 +1,23 @@
-﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
+﻿using FirstMobileApp.Resources;
+using System;
 using Xamarin.Forms;
 
 namespace FirstMobileApp
 {
     public partial class MainPage : ContentPage
     {
+        private bool _isDark = false;
         public MainPage()
         {
             InitializeComponent();
-
-            //some comment to test git
         }
+
+        public bool IsDark { get => _isDark; set => _isDark = value; }
+
+        private void Button_Clicked(object sender, EventArgs e)
+        {
+            Navigation.PushAsync(new Settings(this));
+        }
+
     }
 }
---
+++ FirstMobileApp/FirstMobileApp/Settings.xaml.cs
@@ -0,0 +1,47 @@
+﻿using FirstMobileApp.Resources;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace FirstMobileApp
+{
+    [XamlCompilation(XamlCompilationOptions.Compile)]
+    public partial class Settings : ContentPage
+    {
+        
+        private  MainPage _mainPage;
+        
+        public Settings(MainPage mainPage)
+        {
+            InitializeComponent();
+            _mainPage = mainPage;
+        }
+
+        protected override void OnAppearing()
+        {
+            if (_mainPage.IsDark)
+            {
+                SwitchButton.Toggled -= SwitchButton_Toggled;
+                SwitchButton.IsToggled = true;
+                SwitchButton.Toggled += SwitchButton_Toggled;
+            }
+        }
+
+        private void SwitchButton_Toggled(object sender, ToggledEventArgs e)
+        {
+            var mergedDictionaries = Application.Current.Resources.MergedDictionaries;
+            mergedDictionaries.Clear();
+
+            if (_mainPage.IsDark)
+            {
+                mergedDictionaries.Add(new Light());
+                _mainPage.IsDark = false;
+            }
+            else
+            {
+                mergedDictionaries.Add(new Dark());
+                _mainPage.IsDark = true;
+            }
+        }
+    }
+}
